12bf6d5c9f71c3a74cf38cc771bdde72c6afefe8
can1001 <hyejong.jeong@gmail.com>
2020-10-20 02:24:26 +0900

    테스트 코드 정리

--- src/test/java/com/ncode/goods/ProductServiceTests.java
+++ src/test/java/com/ncode/goods/ProductServiceTests.java
@@ -1,9 +1,6 @@
 package com.ncode.goods;
 
-import com.ncode.goods.domain.entity.product.Product;
 import com.ncode.goods.repository.ProductRepository;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,15 +15,4 @@ public class ProductServiceTests {
     @Autowired
     private ProductRepository productRepository;
 
-    @Test
-    public void saveProductTest() {
-
-        Product product = new Product();
-        product.setProductName("티셔츠 1+1");
-        productRepository.save(product);
-
-        Product retrivedProduct = productRepository.findById(product.getProductId()).get();
-
-        Assert.assertEquals(retrivedProduct.getProductName(), "티셔츠 1+1");
-    }
 }
