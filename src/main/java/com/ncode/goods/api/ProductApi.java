730771843814a52fb5e640a5b9f3ec431c17e076
can1001 <hyejong.jeong@gmail.com>
2020-10-20 02:13:53 +0900

    Swagger UI 2 setting
    http://localhost:8080/swagger-ui.html#/

--- src/main/java/com/ncode/goods/api/ProductApi.java
+++ src/main/java/com/ncode/goods/api/ProductApi.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
-
 @Api
 @RestController
 @RequestMapping("/v1/api/product")
